62d430ae2acc6973ddbd874a6738c2921adfaf59
Chris Shepard <imajunryou@gmail.com>
2017-08-21 20:54:21 -0600

    Debugging work, update readme to match

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Work done for RealPython, part 2.
 
 - Flask: Quick Start -- *Complete*
 - Interlude: Database Programming -- *Complete*
-- Flask: Blog App -- **In Progress** (Just begninning)
+- Flask: Blog App -- *Complete*
+- Interlude: Debugging -- *Complete*
+- Flask: FlaskTaskr, Part 1 - Quick Start -- **In Progress** (Just starting)
---
+++ debugging/pdb_ex.py
@@ -0,0 +1,30 @@
+import pdb
+import sys
+from random import choice
+
+random1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+random2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+while True:
+    print("To exit this game, type 'exit'")
+    num1 = choice(random2)
+    num2 = choice(random1)
+
+    answer = input("What is {} times {}? ".format(num1, num2))
+
+    # exit
+    if answer == "exit":
+        print("Now exiting game!")
+        sys.exit()
+
+    try:
+        answer = int(answer)
+    except ValueError:
+        print("That's not an integer!")
+        continue
+
+    # determine if number is correct
+    if answer == num1 * num2:
+        print("Correct!")
+    else:
+        print("Wrong!")
